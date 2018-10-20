7c3b5f6b395245a2acfb24ad15f9b924db1fec89
HPELITEBOOK6930\User <wolver2ne89@mail.ru>
2018-10-20 18:05:13 +0300

    Добавил программу консольную.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static object print;
+
+        static void Main(string[] args)
+        {
+            print "Hello world";
+        }
+    }
+}
